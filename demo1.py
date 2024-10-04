eaaa5c05ce579367c0d5fb95e02686c0312bcfec
amal-k04 <amal17philip@gmail.com>
2024-10-04 16:05:20 +0530

    Add files via upload

---
+++ demo1.py
@@ -0,0 +1,47 @@
+#print("hello, world!")
+#print("hello,\n world!")
+'''x=5
+y="amal"
+print(x)
+print(y)
+
+x=10
+y='amal'
+print(type(x))
+print(type(y))
+
+
+a="hello"
+b="world"
+c=a+b
+print(c)
+
+'''
+
+'''
+x=y=z="orange"
+print(y)
+print(x)
+print(z)
+
+
+x,y,z="Orange","banana","cherry"
+
+print(x)
+print(y)
+print(z)
+
+x="python"
+y="is"
+z="awesome"
+print(x + y + z)
+
+
+n=input('enter your name:')
+print("username is :",n)
+
+a="hello, world!"
+print(a[2])
+'''
+b='hello, world!'
+print(b[4:])
