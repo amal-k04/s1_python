cffba0495cff02ab05b94ed4140bc39ef77e29a2
amal-k04 <amal17philip@gmail.com>
2024-10-18 16:10:30 +0530

    Add files via upload

---
+++ basicin_mathmodule.py
@@ -0,0 +1,13 @@
+'''
+Author:Amal k philip
+Date:18-10-2024
+description:uses functions from the math module'''
+
+import math
+sq_num=int(input('enter square root of:'))
+fac=int(input("enter the factorial :"))
+po_num=int(input("enter the base number:"))
+ex_num=int(input("enter the exponential:"))
+print("square root of ",sq_num,":",math.sqrt(sq_num))
+print("the factorial of ",fac,":",math.factorial(fac))
+print(po_num,"raised to power",ex_num,":",math.pow(po_num,ex_num))
