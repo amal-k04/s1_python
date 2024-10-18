b3dd27de2441c4a4d56e3b4db0a9ede7690f98d6
amal-k04 <amal17philip@gmail.com>
2024-10-18 16:23:43 +0530

    Add files via upload

---
+++ string_format1.py
@@ -0,0 +1,6 @@
+"""
+Author:Amal k philip
+Date:18-10-2024
+description:sting in specific format"""
+sample1="Twinkle, twinkle, little star,\n\t\tHow I wonder what you are! \n\t\t\tUp above the world so high,\n\t\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star, \n\t\tHow I wonder what you are"
+print(sample1)
