e1368fae2b974cd6a2cf983c76bd27f3b9696868
amal-k04 <amal17philip@gmail.com>
2024-10-18 14:34:09 +0530

    Add files via upload

---
+++ baseop1.py
@@ -0,0 +1,12 @@
+'''
+Author:Amal k philip
+Date:18-10-2024
+Description:program that asks the user to input
+their name and age. After receiving the input,
+display a message that includes the user's name and age.
+Hello, amal You are 18 years old.
+'''
+
+name1=input(str("enter your name :"))
+age1=input("enter your age :")
+print("Hello,",name1+" You are",age1,"years old.")
