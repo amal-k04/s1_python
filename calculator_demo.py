ed6fd3e9a4be3351ae4eba702bd2ea0e2cc912c3
AMAL K PHILIP <amal17philip@gmail.com>
2024-10-19 14:29:45 +0530

    Add files via upload

---
+++ calculator_demo.py
@@ -0,0 +1,20 @@
+'''
+Author:Amal k philip
+Date:19-10-2024
+description:BASIC CALCULATOR AND COMBINED OPERATION'''
+
+num_1=int(input("enter your first number:"))
+num_2=int(input("enter your second number:"))
+num_3=int(input("enter your first number:"))
+sum_=num_1+num_2
+print("The sum of num1 and num2 is:", sum_)
+diff=num_1-num_2
+print("The difference when num2 is subtracted from num1 is:",diff)
+prod=num_1*num_2
+print("The quotient when num1 is divided by num2 is:",prod)
+div=num_1/num_2
+print("The quotient when num1 is divided by num2 is:",div)
+mod=num_1%num_2
+print("The remainder when num1 is divided by num2 is:",mod)
+result=(num_1+num_2)*num_3/2
+print("The result of (num1 + num2) * num3 / 2 is:",result)
