c8db9f1b6562b41c42a64ee81217e8570b7bb5db
AMAL K PHILIP <amal17philip@gmail.com>
2024-10-19 16:30:11 +0530

    concate_string_demo.py

--- concate_string_demo.py
+++ concate_string_demo.py
@@ -3,8 +3,8 @@ Author:Amal k philip
 Date:19-10-2024
 description:concatenating the string '''
 
-f_name=input("enter your first number:")
+f_name=input("enter your first name:")
 l_name=input("enter you last name:")
 result=f_name +" "+l_name
 print(result)
-print("the sub_string is :",result[:len(f_name)+1])
+print("the sub_string is :",result[len(f_name):])
