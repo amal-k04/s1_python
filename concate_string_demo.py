fba300036d3777d292a320214ed028de22dc10fd
AMAL K PHILIP <amal17philip@gmail.com>
2024-10-19 14:55:07 +0530

    Add files via upload

---
+++ concate_string_demo.py
@@ -0,0 +1,10 @@
+'''
+Author:Amal k philip
+Date:19-10-2024
+description:concatenating the string '''
+
+f_name=input("enter your first number:")
+l_name=input("enter you last name:")
+result=f_name +" "+l_name
+print(result)
+print("the sub_string is :",l_name[0:])
