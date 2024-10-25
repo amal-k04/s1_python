05caebe912cec2119532d5b2193c597e1799561b
AMAL K PHILIP <amal17philip@gmail.com>
2024-10-25 14:37:16 +0530

    Add files via upload

---
+++ compara_two_number.py
@@ -0,0 +1,11 @@
+'''
+Author:Amal k philip
+Date:25-10-2024
+description:compare two numbers
+'''
+num_1=int(input("Enter your number:"))
+num_2=int(input("Enter your number:"))
+if num_1>num_2:
+    print("the frst number is greater.",num_1)
+else:
+    print("the second number is greater.",num_2)
